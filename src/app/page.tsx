a240dc47392f7ab74ab21512fce9d5ff83a2e500
Dereje-boy <Derejeg35@gmail.com>
2024-04-01 15:11:28 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen flex-col">
+        <h1>Hello world</h1>
+      <button>Click Me</button>
+    </main>
+  )
+}
